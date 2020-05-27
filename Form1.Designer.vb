1eca2facd6ff53aa53abf09fabd817fe1e0cea78
Nadia1944 <63362943+Nadia1944@users.noreply.github.com>
2020-05-27 15:19:54 +0700

    aaa

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -28,7 +28,7 @@ Partial Class Menu_Utama
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(554, 310)
         Me.Name = "Menu_Utama"
         Me.Text = "Menu_Utama"
         Me.ResumeLayout(False)
