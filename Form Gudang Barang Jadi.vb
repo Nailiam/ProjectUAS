a19fc5c0fa4599e35fd5692dcafb7567e9fc3d71
Nailiam <nnailiamalia@gmail.com>
2020-06-11 18:59:53 +0700

    Tijel3

--- Form Gudang Barang Jadi.vb
+++ Form Gudang Barang Jadi.vb
@@ -1,7 +1,7 @@
 ﻿Public Class Form_Gudang_Barang_Jadi
     Private GudangBJ As New Menu_Utama
     Private Sub Btnstok_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Btnstok.Click
-        GudangBJ.Stok_badan1 = GudangBJ.Stok_badan1 + GudangBJ.Pro_badan1 - GudangBJ.Log_badan1
+        GudangBJ.Stok_sport = GudangBJ.Stok_sport + GudangBJ.Pro_sport - Form_Logistik.txtsport.Text
         GudangBJ.Stok_pintu1 = GudangBJ.Stok_pintu1 + GudangBJ.Pro_pintu1 - GudangBJ.Log_pintu1
         GudangBJ.Stok_roda1 = GudangBJ.Stok_roda1 + GudangBJ.Pro_roda1 - GudangBJ.Log_roda1
         GudangBJ.Stok_kursi1 = GudangBJ.Stok_kursi1 + GudangBJ.Pro_kursi1 - GudangBJ.Log_kursi1
@@ -14,7 +14,7 @@
     End Sub
 
     Private Sub Btninfokeluar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Btninfokeluar.Click
-        ListView3.Items.Add(New ListViewItem(New String() {GudangBJ.Log_badan1, GudangBJ.Log_pintu1, GudangBJ.Log_roda1, GudangBJ.Log_kursi1, GudangBJ.Log_mesin1}))
+        ListView3.Items.Add(New ListViewItem(New String() {clsPemesanan.database(0).mobilsedan, clsPemesanan.database(0).mobilsport}))
     End Sub
 
     Private Sub Btnmenu_Click(sender As Object, e As EventArgs) Handles btnmenu.Click
