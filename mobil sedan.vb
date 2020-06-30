93fba65d0dc7ca4a9c5a2c2600fc381c78437c87
Nadia1944 <63362943+Nadia1944@users.noreply.github.com>
2020-06-30 18:51:19 +0700

    produksi

--- mobil sedan.vb
+++ mobil sedan.vb
@@ -1,37 +1,27 @@
 ﻿Public Class mobil_sedan
     Inherits Produksi
     Public Function produksikerangkabadan(ByVal jumlah_produksi As Integer) As String
-        For Each pesan In clsPemesanan.database
-            jumlah_produksi = pesan.mobilsedan
-        Next
+        jumlah_produksi = Form_Produksi.txtjmlsport.Text
         produksikerangkabadan = Form_Produksi.Pro_badan.Text
         Return (jumlah_produksi * 1)
     End Function
     Public Function produksikerangkapintu(ByVal jumlah_produksi As Integer) As String
-        For Each pesan In clsPemesanan.database
-            jumlah_produksi = pesan.mobilsedan
-        Next
+        jumlah_produksi = Form_Produksi.txtjmlsport.Text
         produksikerangkapintu = Form_Produksi.Pro_pintu.Text
         Return (jumlah_produksi * 4)
     End Function
     Public Function produksiroda(ByVal jumlah_produksi As Integer) As String
-        For Each pesan In clsPemesanan.database
-            jumlah_produksi = pesan.mobilsedan
-        Next
+        jumlah_produksi = Form_Produksi.txtjmlsport.Text
         produksiroda = Form_Produksi.Pro_roda.Text
         Return (jumlah_produksi * 4)
     End Function
     Public Function produksikursi(ByVal jumlah_produksi As Integer) As String
-        For Each pesan In clsPemesanan.database
-            jumlah_produksi = pesan.mobilsedan
-        Next
+        jumlah_produksi = Form_Produksi.txtjmlsport.Text
         produksikursi = Form_Produksi.Pro_kursi.Text
         Return (jumlah_produksi * 5)
     End Function
     Public Function produksimesin(ByVal jumlah_produksi As Integer) As String
-        For Each pesan In clsPemesanan.database
-            jumlah_produksi = pesan.mobilsedan
-        Next
+        jumlah_produksi = Form_Produksi.txtjmlsport.Text
         produksimesin = Form_Produksi.Pro_mesin.Text
         Return (jumlah_produksi * 1)
     End Function
--- mobil sport.vb
+++ mobil sport.vb
@@ -1,37 +1,27 @@
 ﻿Public Class mobil_sport
     Inherits Produksi
     Public Function produksikerangkabadan1(ByVal jumlah_produksi As Integer) As String
-        For Each pesan In clsPemesanan.database
-            jumlah_produksi = pesan.mobilsport
-        Next
+        jumlah_produksi = Form_Produksi.txtjmlsport.Text
         produksikerangkabadan1 = Form_Produksi.txtbadansport.Text
         Return (jumlah_produksi * 1)
     End Function
     Public Function produksikerangkapintu1(ByVal jumlah_produksi As Integer) As String
-        For Each pesan In clsPemesanan.database
-            jumlah_produksi = pesan.mobilsport
-        Next
+        jumlah_produksi = Form_Produksi.txtjmlsport.Text
         produksikerangkapintu1 = Form_Produksi.txtpintusport.Text
         Return (jumlah_produksi * 2)
     End Function
     Public Function produksiroda1(ByVal jumlah_produksi As Integer) As String
-        For Each pesan In clsPemesanan.database
-            jumlah_produksi = pesan.mobilsport
-        Next
+        jumlah_produksi = Form_Produksi.txtjmlsport.Text
         produksiroda1 = Form_Produksi.txtrodasport.Text
         Return (jumlah_produksi * 4)
     End Function
     Public Function produksikursi1(ByVal jumlah_produksi As Integer) As String
-        For Each pesan In clsPemesanan.database
-            jumlah_produksi = pesan.mobilsport
-        Next
+        jumlah_produksi = Form_Produksi.txtjmlsport.Text
         produksikursi1 = Form_Produksi.txtkursisport.Text
         Return (jumlah_produksi * 2)
     End Function
     Public Function produksimesin1(ByVal jumlah_produksi As Integer) As String
-        For Each pesan In clsPemesanan.database
-            jumlah_produksi = pesan.mobilsport
-        Next
+        jumlah_produksi = Form_Produksi.txtjmlsport.Text
         produksimesin1 = Form_Produksi.txtmesinsport.Text
         Return (jumlah_produksi * 1)
     End Function
